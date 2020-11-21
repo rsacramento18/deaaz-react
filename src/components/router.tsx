3e4db4753b0a82b2db76a2d882f624c023579237
Ricardo Sacramento <ricardo_sacramento@outlook.com>
2020-11-21 08:24:04 +0000

    starting css

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
-import Home from '../pages/home/home';
-import About from '../pages/about/about';
-import Posts from '../pages/posts/posts';
+import Home from '../pages/home';
+import About from '../pages/about';
+import Posts from '../pages/posts';
+import Writer from '../pages/writer';
 
 const AppRouter = () => {
     return (
         <Router>
-            <div>
+            <div className="logo">
+                <h1>De A a Z</h1>
+            </div>
+            <div className="menu">
                 <nav>
                     <ul>
                         <li>
                             <Link to="/">Home</Link>
                         </li>
                         <li>
-                            <Link to="/about">About</Link>
+                            <Link to="/about">Sobre</Link>
                         </li>
                         <li>
                             <Link to="/posts">Posts</Link>
+                        </li>
+                        <li>
+                            <Link to="/write">Área Escritor</Link>
                         </li>
                     </ul>
                 </nav>
                 <Route path="/" exact component={ Home }></Route>
                 <Route path="/about" exact component={ About }></Route>
                 <Route path="/posts" exact component={ Posts }></Route>
+                <Route path="/posts" exact component={ Writer}></Route>
             </div>
         </Router>
     );
---
+++ src/pages/writer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Writer = () => {
+  
+  return (
+    <div>
+      Writer Area
+    </div>
+  );
+}
+
+export default Writer;
