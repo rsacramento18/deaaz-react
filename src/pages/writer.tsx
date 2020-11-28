d36434ec326e3e518f8023d89b3559789c306a48
rsacramento18 <ricardo_sacramento@outlook.com>
2020-11-28 18:43:44 +0000

    Changes to writer page

--- src/pages/writer.tsx
+++ src/pages/writer.tsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { SketchPicker } from "react-color";
 
 const Writer = () => {
 
     const { register, handleSubmit } = useForm();
 
-    const [loggedIn, setLoggedIn ] = useState(false);
+    const [loggedIn, setLoggedIn ] = useState(true);
 
-    const user = {
+    const [colorPicker, setColorPicker] = useState("none");
+    const [posts, setPosts] = useState("none");
+
+    const plusLogo = <FontAwesomeIcon icon={faPlusCircle} />
+    const deleteLogo = <FontAwesomeIcon icon={faTrashAlt} />
+
+    const user: any = {
         name: 'Ricardo Sacramento',
         email: 'ricardo.sacramento18@gmail.com',
         alias: [
             {
                 letter: 'L',
-                description: 'Some description about the alias L',
+                color: '#fabd2f',
+                description: 'Some description about the alias L, and i need to make it bigger in oder to see the overflow effect.Some description about the alias L, and i need to make it bigger in oder to see the overflow effect.',
                 posts: [
                     {
                         title: 'Some Post Title',
@@ -37,6 +47,7 @@ const Writer = () => {
             },
             {
                 letter: 'M',
+                color: '#fb4934',
                 description: 'Writing some texts related to the story of life',
                 posts: [
                     {
@@ -67,59 +78,76 @@ const Writer = () => {
         setLoggedIn(true);
     }
 
+
+    const toggleShowColor = () => {
+        if (colorPicker === "none") {
+            setColorPicker("block");
+        }
+        else {
+            setColorPicker("none")
+        }
+    }
+    
+    const showPosts = (index : number) => {
+        if (colorPicker === "none") {
+            setColorPicker("block");
+        }
+        else {
+            setColorPicker("none")
+        }
+    }
+
     if ( loggedIn ) {
         return (
             <div className="writer">
                 <div className="section-title">
                     <h1>área de escritor</h1>
                 </div>
+
+                {/* <div className="colorPicker" style={{display: colorPicker}}> */}
+                {/*     <SketchPicker /> */}
+                {/* </div> */}
                 <div className="settings">
-                    <div className="alias">
-                        <div className="alias-letter">
-                            <span>L</span>
-                        </div>
-                        <div className="alias-description">
-                            <span>Some description for the alias</span>
-                        </div>
-                        <div className="alias-posts">
-                            <input type="button" value="+"/>
-                        </div>
-                        <div className="alias-delete">
-                            <input type="button" value="delete"/>
-                        </div>
-                    </div>
-                    <div className="alias-posts">
-                        <div className="alias-post-title">
-                            <span>Some post title</span>
-                        </div>
-                        <div className="alias-date">
-                            <span>18/05/2020</span>
-                        </div>
-                        <div className="alias-posts">
-                            <input type="button" value="+"/>
-                        </div>
-                        <div className="alias-delete">
-                            <input type="button" value="delete"/>
-                        </div>
-                    </div>
+                    { user.alias.map((alias: any, index: number) => {
+                        return (
+                            <div key={index} className="alias">
+                                <div className="alias-settings">
+                                    <div className="alias-letter">
+                                        <span>{alias.letter}</span>
+                                    </div>
+                                    <div className="alias-color" onClick={toggleShowColor} style={{backgroundColor: alias.color}}>
+                                    </div>
+                                    <div className="alias-description">
+                                        <span>{alias.description}</span>
+                                    </div>
+                                    <div className="alias-options">
+                                        <span className="moreBt" onClick={() => showPosts(index)}>{ plusLogo }</span>
+                                        <span className="deleteBt">{ deleteLogo }</span>
+                                    </div>
+                                </div>
+                                <div key={index} className="alias-posts">
+                                    { alias.posts.map((post: any, index: number) => {
+                                        return (
+                                            <div key={index} className="alias-post">
+                                                <div className="alias-post-info">
+                                                    <p className="alias-post-info-title">{ post.title }</p>
+                                                    <p className="alias-post-info-subtitle">{ post.subtitle }</p>
+                                                    <p className="alias-post-info-date">{ post.date }</p>
+                                                </div>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+
+                        )
+                    })}
                     <div className="user">
-                        <div>
-                            <label>Nome</label>
-                            <span>Ricardo Sacramento</span>
+                        <div className="user-name">
+                            <span>{ user.name }</span>
                         </div>
-                        <div>
-                            <label>email</label>
-                            <span>ricardo.sacramento18@gmail.com</span>
-                        </div>
-                    </div>
-                    <div className="alias">
-                        <div>
-                            <label>Letra</label>
-                            <span>R</span>
-                        </div>
-                        <div>
-                            <label>Cor</label>
-                            <span>Yellow</span>
+                        <div className="user-email">
+                            <span>{ user.email }</span>
                         </div>
                     </div>
                 </div>
